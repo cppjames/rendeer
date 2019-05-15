2dabc668f6d027108facafb7d7da0f2fd9c6554b
Forfunckle <cosminjames555@gmail.com>
2019-05-15 15:18:05 +0300

    Version 3.0.0

--- settings.py
+++ settings.py
@@ -1,26 +1,31 @@
 from enum import Enum
 
-ShadingMode = Enum('ShadingMode', 'unlit flatDiffuse wireframe') #shading modes (don't modify this)
+ShadingMode = Enum('ShadingMode', 'unlit flatDiffuse wireframe gouraud') #shading modes (don't modify this)
 
-yRotation = 70 #rotation of the mesh on the Y axis (in degrees)
-xRotation = 90 #rotation of the mesh on the X axis (in degrees)
-zRotation = 0 #rotation of the mesh on the Z axis (in degrees)
+imageResolution = 350
+
+yRotation = 125 #rotation of the mesh on the Y axis (in degrees)
+xRotation = 0 #rotation of the mesh on the X axis (in degrees)
+zRotation = 180 #rotation of the mesh on the Z axis (in degrees)
 
 lightDirectionX = 0 #rotation of the light source (in degrees)
 lightDirectionY = -50 #rotation of the light source (in degrees)
 lightDirectionZ = -20 #rotation of the light source (in degrees)
 
-shadingMode = ShadingMode.flatDiffuse
+shadingMode = ShadingMode.gouraud
 
-calculateVertexNormals = False
+calculateVertexNormals = True
 
-diffuseColor = [20, 10, 170] #diffuse color on the lightest face of the mesh
+backfaceCulling = False
+
+diffuseColor = [0, 255, 170] #diffuse color on the lightest face of the mesh
 unlitColor = [10, 200, 40] #color that applies to the whole mesh in unlit shading mode
 wireframeColor = [200, 200, 200]
+gouraudColor = [255, 211, 137]
 
 lightVector = [[1, 0, 0]] #initial position of the source of light, relative to the center (0, 0, 0)
     
-meshPosition = [1, 1, -0.3] #position of the mesh. duh
+meshPosition = [1, 0.7, -1.2] #position of the mesh. duh
 
 renderFaceNormals = False #should it render the normals of the faces?
 renderHardVertexNormals = False #should it render the hard normals of the vertices?
